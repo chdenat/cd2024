6d43b8fff08304dcbe1c4a5d40b4e9f5b3862e01
chdenat <christian.denat@orange.fr>
2023-12-30 19:05:11 +0100

    new patterns

--- patterns/menu-haut-boutique.php
+++ patterns/menu-haut-boutique.php
@@ -3,14 +3,16 @@
  * Title: Menu Haut Boutique
  * Slug: cd2024/menu-haut-boutique
  * Categories: cd2024
- * Keywords: columns,title
+ * Keywords: menu
  *
  */
 ?>
-<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"border":{"bottom":{"color":"var:preset|color|secondary","width":"2px"}},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"backgroundColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|primary","width":"3px"}},"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":""}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-base-background-color
      has-background"
-     style="border-bottom-color:var(--wp--preset--color--secondary);border-bottom-width:2px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
-    <!-- wp:site-logo {"width":120} /--><!-- wp:block {"ref":3658} /-->
-    <!-- wp:block {"ref":4019} /--></div>
-<!-- /wp:group -->
+     style="border-bottom-color:var(--wp--preset--color--primary);border-bottom-width:3px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+    <!-- wp:group {"style":{"position":{"type":""},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+    <div class="wp-block-group" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+        <!-- wp:site-logo {"width":90,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+        <!-- wp:block {"ref":3658} /-->
+        <!-- wp:block {"ref":255} /--></div><!-- /wp:group --></div><!-- /wp:group -->
---
+++ patterns/menu-haut.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Menu Haut Boutique
+ * Slug: cd2024/menu-haut
+ * Categories: cd2024
+ * Keywords: menu
+ *
+ */
+?>
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"border":{"bottom":{"color":"var:preset|color|secondary","width":"2px"}},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"backgroundColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-base-background-color
+     has-background"
+     style="border-bottom-color:var(--wp--preset--color--secondary);border-bottom-width:2px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+    <!-- wp:site-logo {"width":120} /--><!-- wp:block {"ref":3658} /-->
+    <!-- wp:block {"ref":4019} /--></div>
+<!-- /wp:group -->
