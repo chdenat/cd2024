f55795b49f6efe3e8b3432db2e7c444746d9b75a
chdenat <christian.denat@orange.fr>
2024-01-03 10:54:45 +0100

    Some enhancement for mobiles

--- patterns/footer.php
+++ patterns/footer.php
@@ -13,8 +13,8 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"100px","bottom":"100px"}}},"backgroundColor":"secondary","className":"noleam--no-top-margin","layout":{"type":"constrained"},"fontSize":"medium","fontFamily":"made-mirage","hideFromFeed":true} -->
 <div class="wp-block-group noleam--no-top-margin has-secondary-background-color has-background has-made-mirage-font-family has-medium-font-size" style="padding-top:100px;padding-bottom:100px"><!-- wp:columns {"fontFamily":"made-mirage"} -->
 <div class="wp-block-columns has-made-mirage-font-family"><!-- wp:column {"verticalAlignment":"center","fontSize":"medium"} -->
-<div class="wp-block-column is-vertically-aligned-center has-medium-font-size"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"},"hideFromFeed":true} -->
-<div class="wp-block-group"><!-- wp:image {"id":14739,"width":"140px","sizeSlug":"full","linkDestination":"none"} -->
+<div class="wp-block-column is-vertically-aligned-center has-medium-font-size"><!-- wp:group {"className":"center-children-on-mobile","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"},"hideFromFeed":true} -->
+<div class="wp-block-group center-children-on-mobile"><!-- wp:image {"id":14739,"width":"140px","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Logo-CD2024-blanc.png" alt="" class="wp-image-14739" style="width:140px"/></figure>
 <!-- /wp:image -->
 
@@ -37,8 +37,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"top"} -->
-<div class="wp-block-column is-vertically-aligned-top"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"constrained"},"hideFromFeed":true} -->
-<div class="wp-block-group has-base-color has-text-color has-link-color"><!-- wp:heading {"level":3} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:group {"metadata":{"name":""},"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","className":"center-children-on-mobile","layout":{"type":"constrained"},"hideFromFeed":true} -->
+<div class="wp-block-group center-children-on-mobile has-base-color has-text-color has-link-color"><!-- wp:heading {"level":3} -->
 <h3 class="wp-block-heading"><a href="https://christinedeloupy.fr/contact/" data-type="page" data-id="87">Me Contacter</a></h3>
 <!-- /wp:heading -->
 
@@ -69,17 +69,7 @@
 <div style="height:3rem" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"3rem"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"medium","hideFromFeed":true} -->
-<div class="wp-block-group has-base-color has-text-color has-link-color has-medium-font-size"><!-- wp:heading {"level":3,"fontSize":"small"} -->
-<h3 class="wp-block-heading has-small-font-size"><a href="/politique-de-confidentialite">Politique de confidentialité</a></h3>
-<!-- /wp:heading -->
-
-<!-- wp:heading {"level":3,"fontSize":"small"} -->
-<h3 class="wp-block-heading has-small-font-size"><a href="https://christinedeloupy.fr/mentions-legales/" data-type="page" data-id="7065">Mentions légales</a></h3>
-<!-- /wp:heading -->
-
-<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
-<h3 class="wp-block-heading has-base-color has-text-color has-link-color has-small-font-size">Conditions générales de Vente</h3>
-<!-- /wp:heading --></div>
+<!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|base","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"},"hideFromFeed":true} -->
+<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--base);border-top-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:navigation {"ref":13455,"textColor":"base","overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"var:preset|spacing|80"}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
--- patterns/header-home-page.php
+++ patterns/header-home-page.php
@@ -10,14 +10,22 @@
 
 <!-- wp:cover {"overlayColor":"secondary","isUserOverlayColor":true,"minHeight":80,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"metadata":{"name":""},"style":{"spacing":{"blockGap":"2rem"},"color":[],"border":{"width":"0px","style":"none","radius":"0px"}},"className":"rounded-on-mobile","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover is-light rounded-on-mobile" style="border-style:none;border-width:0px;border-radius:0px;min-height:80vh"><span aria-hidden="true" class="wp-block-cover__background has-secondary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":14571,"width":"434px","height":"auto","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","align":"right","style":{"border":{"width":"6px","color":"#fff","radius":{"topLeft":"20rem","topRight":"20rem","bottomLeft":"20rem","bottomRight":"2rem"}}},"className":"has-lightbox"} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","className":"center-children-on-mobile"} -->
+<div class="wp-block-column is-vertically-aligned-center center-children-on-mobile"><!-- wp:spacer {"height":"80px","className":"hide-on-desktop"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer hide-on-desktop"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":14571,"width":"434px","height":"auto","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","align":"right","style":{"border":{"width":"6px","color":"#fff","radius":{"topLeft":"20rem","topRight":"20rem","bottomLeft":"20rem","bottomRight":"2rem"}}},"className":"has-lightbox"} -->
 <figure class="wp-block-image alignright size-full is-resized has-custom-border has-lightbox"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Design-sans-titre-35.jpg" alt="" class="has-border-color wp-image-14571" style="border-color:#fff;border-width:6px;border-top-left-radius:20rem;border-top-right-radius:20rem;border-bottom-left-radius:20rem;border-bottom-right-radius:2rem;aspect-ratio:1;object-fit:cover;width:434px;height:auto"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"base","fontSize":"xl","fontFamily":"made-mirage"} -->
+<!-- wp:column {"verticalAlignment":"center","className":"center-children-on-mobile"} -->
+<div class="wp-block-column is-vertically-aligned-center center-children-on-mobile"><!-- wp:spacer {"height":"80px","className":"hide-on-desktop"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer hide-on-desktop"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"base","fontSize":"xl","fontFamily":"made-mirage"} -->
 <p class="has-base-color has-text-color has-link-color has-made-mirage-font-family has-xl-font-size has-custom-font" style="font-style:normal;font-weight:600;font-family:made-mirage">Je suis Christine Deloupy,</p>
 <!-- /wp:paragraph -->
 
@@ -37,7 +45,11 @@
 <p class="has-base-color has-text-color has-link-color has-made-mirage-font-family has-small-font-size has-custom-font" style="font-family:made-mirage">Durée 30 minutes</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons {"layout":{"type":"flex"}} -->
+<!-- wp:spacer {"height":"60px","className":"hide-on-desktop"} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer hide-on-desktop"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
 <div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","textColor":"base","style":{"border":{"radius":"2rem"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"className":"is-style-outline"} -->
 <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-base-color has-text-color has-link-color has-text-align-center wp-element-button" href="https://christinedeloupy.fr/seance-clarte/" style="border-radius:2rem">Séance clarté</a></div>
 <!-- /wp:button -->
@@ -45,7 +57,11 @@
 <!-- wp:button {"textAlign":"center","textColor":"base","style":{"border":{"radius":"2rem"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"className":"is-style-outline"} -->
 <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-base-color has-text-color has-link-color has-text-align-center wp-element-button" href="https://christinedeloupy.fr/je-suis/" style="border-radius:2rem">En savoir Plus</a></div>
 <!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"60px","className":"hide-on-desktop"} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer hide-on-desktop"></div>
+<!-- /wp:spacer --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div></div>
 <!-- /wp:cover -->
